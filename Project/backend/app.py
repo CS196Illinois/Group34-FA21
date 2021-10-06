3ef019728f54af7924c28dbe82b5f9aa3b1c2c86
svijay77 <sarvagnyav@gmail.com>
2021-10-06 15:41:22 -0500

    organized backend files, wrote data modeling framework

---
+++ Project/backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+#from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+"""app.config["SQL_ALCHEMY_URI"] = "mysql+pymysql://admin:$mysql5467V@localhost/greenthumb"
+db = SQLAlchemy(app)"""
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, World!</h1>"
