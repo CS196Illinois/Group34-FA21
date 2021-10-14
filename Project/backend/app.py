54e370b63101f7aa7fc8afd668214543ead02b15
svijay77 <sarvagnyav@gmail.com>
2021-10-14 01:24:27 -0500

    setup firebase realtime db and saved sample json

--- Project/backend/app.py
+++ Project/backend/app.py
@@ -1,10 +1,38 @@
+import firebase_admin
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import uuid
+import os
+from firebase_admin import credentials, db, firestore, initialize_app
 
+load_dotenv()
+token = os.environ.get('GOOGLE_APP_CRED')
 app = Flask(__name__)
-app.config["SQL_ALCHEMY_URI"] = "mysql+pymysql://admin:$mysql5467V@localhost/greenthumb"
-db = SQLAlchemy(app)
+
+#initializing firebase obj
+cred = firebase_admin.credentials.Certificate(token)
+default_app = firebase_admin.initialize_app(cred, {"databaseURL": "https://greenthumb-9d393-default-rtdb.firebaseio.com"})
+
+ref = db.reference()
+
+#sample json
+listings_ref = ref.child('listings')
+listings_ref.set({
+    0: {
+        "name": "apple",
+        "price": 0.50,
+        "seller": "Joe Brown",
+        "zipcode": 61801,
+    },
+    1: {
+        "name": "apple",
+        "price": 0.50,
+        "seller": "Joe Brown",
+        "zipcode": 61801,
+    }
+})
 
 @app.route("/home", methods = ['GET'])
 def main_page():
-    return "hello"
+    print(ref.get())
+    return ref.get()
