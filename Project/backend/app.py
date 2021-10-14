10362c67dccb656b3b7e1db238174993dc7b1431
svijay77 <sarvagnyav@gmail.com>
2021-10-14 01:39:26 -0500

    removed json from the directory LOL

--- Project/backend/app.py
+++ Project/backend/app.py
@@ -7,28 +7,31 @@ from firebase_admin import credentials, db, firestore, initialize_app
 
 load_dotenv()
 TOKEN = os.environ.get('GOOGLE_APP_CRED')
+PATH = os.environ.get('DATABASEURL')
 app = Flask(__name__)
 
 #initializing firebase obj
 cred = firebase_admin.credentials.Certificate(TOKEN)
-default_app = firebase_admin.initialize_app(cred, {"databaseURL": "https://greenthumb-9d393-default-rtdb.firebaseio.com"})
+default_app = firebase_admin.initialize_app(cred, {"databaseURL": str(PATH)})
 
 ref = db.reference()
 
 #sample json
 listings_ref = ref.child('listings')
 listings_ref.set({
-    0: {
+   0 : {
+        "id": "9384",
         "name": "apple",
         "price": 0.50,
         "seller": "Joe Brown",
-        "zipcode": 61801,
+        "zipcode": 61801
     },
-    1: {
+   1: {
+        "id": "0909",
         "name": "apple",
         "price": 0.50,
         "seller": "Joe Brown",
-        "zipcode": 61801,
+        "zipcode": 61801
     }
 })
 
