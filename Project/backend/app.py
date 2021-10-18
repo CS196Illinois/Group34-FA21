9b1b64d8be4ab702ef3de928d053eabaef85bcbe
svijay77 <sarvagnyav@gmail.com>
2021-10-18 01:00:13 -0500

    returns json for home page

--- Project/backend/app.py
+++ Project/backend/app.py
@@ -1,13 +1,15 @@
 import firebase_admin
-from flask import Flask
+from flask import Flask, jsonify
 from dotenv import load_dotenv
 import uuid
 import os
+import json
 from firebase_admin import credentials, db, firestore, initialize_app
 
 load_dotenv()
-TOKEN = os.environ.get('GOOGLE_APP_CRED')
+TOKEN = os.environ.get("GOOGLE_APP_CRED")
 PATH = os.environ.get('DATABASEURL')
+
 app = Flask(__name__)
 
 #initializing firebase obj
@@ -17,25 +19,12 @@ default_app = firebase_admin.initialize_app(cred, {"databaseURL": str(PATH)})
 ref = db.reference()
 
 #sample json
+f = open('listings.json')
 listings_ref = ref.child('listings')
-listings_ref.set({
-   0 : {
-        "id": "9384",
-        "name": "apple",
-        "price": 0.50,
-        "seller": "Joe Brown",
-        "zipcode": 61801
-    },
-   1: {
-        "id": "0909",
-        "name": "apple",
-        "price": 0.50,
-        "seller": "Joe Brown",
-        "zipcode": 61801
-    }
-})
+listings_ref.set(json.load(f))
+f.close()
 
 @app.route("/home", methods = ['GET'])
 def main_page():
     print(ref.get())
-    return ref.get()
+    return jsonify(ref.get())
